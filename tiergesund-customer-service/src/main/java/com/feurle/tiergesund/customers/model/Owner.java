2f3525967504b1bf63f7dd1eec47615d4eaec342
Daniel Feurle <feurle@e.email>
2023-02-16 21:16:16 +0100

    Add customer service

---
+++ tiergesund-customer-service/src/main/java/com/feurle/tiergesund/customers/model/Owner.java
@@ -0,0 +1,103 @@
+package com.feurle.tiergesund.customers.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.core.style.ToStringCreator;
+
+/**
+ * Simple JavaBean domain object representing an owner.
+ */
+@Entity
+@Table(name = "owners")
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Integer id;
+
+    @Getter
+    @Setter
+    @Column(name = "first_name")
+    @NotBlank
+    private String firstName;
+
+    @Getter
+    @Setter
+    @Column(name = "last_name")
+    @NotBlank
+    private String lastName;
+
+    @Getter
+    @Setter
+    @Column(name = "address")
+    @NotBlank
+    private String address;
+
+    @Getter
+    @Setter
+    @Column(name = "city")
+    @NotBlank
+    private String city;
+
+    @Getter
+    @Setter
+    @Column(name = "telephone")
+    @NotBlank
+    @Digits(fraction = 0, integer = 12)
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "owner")
+    private Set<Pet> pets;
+
+    protected Set<Pet> getPetsInternal() {
+        if (this.pets == null) {
+            this.pets = new HashSet<>();
+        }
+        return this.pets;
+    }
+
+    public List<Pet> getPets() {
+        final List<Pet> sortedPets = new ArrayList<>(getPetsInternal());
+        PropertyComparator.sort(sortedPets, new MutableSortDefinition("name", true, true));
+        return Collections.unmodifiableList(sortedPets);
+    }
+
+    public void addPet(Pet pet) {
+        getPetsInternal().add(pet);
+        pet.setOwner(this);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this)
+
+            .append("id", this.getId())
+            .append("lastName", this.getLastName())
+            .append("firstName", this.getFirstName())
+            .append("address", this.address)
+            .append("city", this.city)
+            .append("telephone", this.telephone)
+            .toString();
+    }
+}
