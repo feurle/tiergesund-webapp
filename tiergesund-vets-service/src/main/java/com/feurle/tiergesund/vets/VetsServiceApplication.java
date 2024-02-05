b6a1f74718b58358550021afa2fe995b3e849d4d
Daniel Feurle <daniel@feurle.com>
2024-02-05 08:07:12 +0100

    SOme checkstyle fixes

--- tiergesund-vets-service/src/main/java/com/feurle/tiergesund/vets/VetsServiceApplication.java
+++ tiergesund-vets-service/src/main/java/com/feurle/tiergesund/vets/VetsServiceApplication.java
@@ -1,17 +1,22 @@
 package com.feurle.tiergesund.vets;
 
+import com.feurle.tiergesund.vets.system.VetsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import com.feurle.tiergesund.vets.system.VetsProperties;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(VetsProperties.class)
 public class VetsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VetsServiceApplication.class, args);
-	}
+    /**
+     * Uses Spring Boot's run method to launch this VetsService application.
+     *
+     * @param args application arguments for startup
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(VetsServiceApplication.class, args);
+    }
 }
--- tiergesund-vets-service/src/test/java/com/feurle/tiergesund/vets/web/VetResourceTest.java
+++ tiergesund-vets-service/src/test/java/com/feurle/tiergesund/vets/web/VetResourceTest.java
@@ -1,13 +1,13 @@
 package com.feurle.tiergesund.vets.web;
 
+import com.feurle.tiergesund.vets.model.Vet;
+import com.feurle.tiergesund.vets.model.VetRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import com.feurle.tiergesund.vets.model.Vet;
-import com.feurle.tiergesund.vets.model.VetRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
